7187ca6dd0a6c4b29b2e7948c427c01ffa512a08
thedevprincee <thedevprince@gmail.com>
2023-01-16 17:09:45 -0800

    first algorthm

--- algorthm.js
+++ algorthm.js
@@ -1,38 +1,53 @@
-const capTitleCase = (str) => {
-  const word = str.split(" ")
-  let newWord = [] 
-  for(let i=0; i<word.length; i++){
-    newWord.push((word[i][0].toUpperCase()+""+word[i].slice(1).toLowerCase()))
+const check = document.getElementById("check")
+const displaySentence = document.getElementById("display_sentence")
+const displayVowel = document.getElementById("display_vowel")
+const displayConsonant = document.getElementById("display_consonant")
+const displayLos = document.getElementById("display_los")
+const displayNow = document.getElementById("display_now")
+const lengthVowel = document.getElementById("length_vowel")
+
+check.addEventListener("click", ()=>{
+  const userSentence = document.getElementById("sentence_input").value
+  pickVowelsAndConsonants(userSentence)
+})
+
+const  pickVowelsAndConsonants = (sentence)=> {
+  displaySentence.innerText = sentence
+  const lengthOfSentence = sentence.length
+  console.log(`length Of Sentence: ${lengthOfSentence}`);
+  displayLos.innerText = lengthOfSentence
+
+  const lengthOfWord = sentence.split(" ")
+  console.log(`Number of word: ${lengthOfWord.length}`);
+  displayNow.innerText = lengthOfWord.length
+  
+  const vowels = 'aeiou';
+  const consonants = 'bcdfghjklmnpqrstvwxyz';
+  
+  
+
+  let newVowels = []
+  let newConsonants = []
+  
+  
+  for (let char of sentence.toLowerCase()) {
+    if (vowels.includes(char)) {
+      newVowels.push(char);
+    } else if (consonants.includes(char)) {
+      newConsonants.push(char);
+    }
   }
-  const newWordJoin = newWord.join()
-  const newWordJoinReplace = newWordJoin.replace(/,/g, " ")
-  return newWordJoinReplace
+  
+  console.log(`Number of Vowel:: ${newVowels.length}`);
+  displayVowel.innerText = newVowels
+  lengthVowel.innerText = newVowels.length
+
+  
+  // console.log(`Consonant: ${newConsonants}`);
+  displayConsonant.innerText = newConsonants
 }
 
-console.log(capTitleCase("he is A LOVE child"))
 
+ const sentes = "Tailwind CSS l is the only framework that I've seen scale on large teams. It easy to customize, adapts to any design, and the build size is tiny."
 
-
-// const boowho3 = (bool)=> {
-//   return typeof(bool) === "boolean"
-// }
-// console.log(boowho3(true))
-
-
-// const boowho2 = (bool)=> {
-//   typeof(bool) === "boolean"  ? console.log(true) : console.log(false)
-// }
-// boowho2("red")
-
-
-// const boowho = (bool)=> {
-//   if(bool === true){
-//     return true
-//   }
-//   else if(bool === false){
-//     return true
-//   }
-//   return false
-// }
-
-// console.log(boowho("true"))
+ pickVowelsAndConsonants(sentes)
